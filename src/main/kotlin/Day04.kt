369a665d6eb9272c1474e6a0ea61f1944af4fd37
Stephen Hobson <steve@thehobsons.org.uk>
2020-12-04 21:36:13 +0000

    Cleans up null handling in day 4 solution

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -42,24 +42,24 @@ class Day04(inputFileName: String): Day(inputFileName) {
 
     private fun isValidPart2(passport: HashMap<String, String>): Boolean {
                 // byr (Birth Year) - four digits; at least 1920 and at most 2002.
-        return passport["byr"].toString().toInt() in 1920..2002 &&
+        return passport["byr"]?.toInt() in 1920..2002 &&
                 // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-                passport["iyr"].toString().toInt() in 2010..2020 &&
+                passport["iyr"]?.toInt() in 2010..2020 &&
                 // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-                passport["eyr"].toString().toInt() in 2020..2030 &&
+                passport["eyr"]?.toInt() in 2020..2030 &&
                 // hgt (Height) - a number followed by either cm or in:
                 //     If cm, the number must be at least 150 and at most 193.
                 //     If in, the number must be at least 59 and at most 76.
-                passport["hgt"].toString().let {
+                (passport["hgt"]?:"").let {
                     ("[0-9]{3}cm".toRegex().matches(it) && it.take(3).toInt() in 150..193) ||
                     ("[0-9]{2}in".toRegex().matches(it) && it.take(2).toInt() in 59..76)
                 } &&
                 // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-                "#[0-9,a-f]{6}".toRegex().matches(passport["hcl"].toString()) &&
+                "#[0-9,a-f]{6}".toRegex().matches(passport["hcl"]?:"") &&
                 // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-                "amb|blu|brn|gry|grn|hzl|oth".toRegex().matches(passport["ecl"].toString()) &&
+                "amb|blu|brn|gry|grn|hzl|oth".toRegex().matches(passport["ecl"]?:"") &&
                 // pid (Passport ID) - a nine-digit number, including leading zeroes.
-                "[0-9]{9}".toRegex().matches(passport["pid"].toString())
+                "[0-9]{9}".toRegex().matches(passport["pid"]?:"")
                 // cid (Country ID) - ignored, missing or not.
     }
 }
